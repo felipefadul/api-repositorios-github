bdd9aa9e728269ca5cd2fe7462eb9f2e3d08a27e
Felipe Fadul <felipe.fadul@hotmail.com>
2020-08-02 19:20:03 -0300

    feat: Adiciona página web do projeto

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import * as S from './styled';
+
+export default function Repositories() {
+  const [ repositories, setRepositories ] = useState([]);
+  let indice = 0;
+  
+  useEffect(() => {
+    let repositoriesName = localStorage.getItem('repositoriesName');
+    if (repositoriesName !== null) {
+      repositoriesName = JSON.parse(repositoriesName);
+      setRepositories(repositoriesName);
+      localStorage.clear();
+    } else {
+      useHistory.push('/');
+    }
+  }, []);
+
+  return (
+    <S.Container>
+      <S.Title>Repositórios</S.Title>
+      <S.List>
+        { repositories.map(repository => {
+          indice++;
+          return (
+            <S.ListItem>Repositório { indice }: { repository }</S.ListItem>
+          );
+        }) }
+      </S.List>
+      <S.LinkHome to='/'>Voltar</S.LinkHome>
+    </S.Container>
+  )
+}
